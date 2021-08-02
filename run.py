de279260b280e242510de5efd934816e8cbdbfb5
Koko-66 <52763151+Koko-66@users.noreply.github.com>
2021-08-02 09:45:25 +0100

    Add welcome message

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+"""
+Settings from setting up Google sheet are taken from Code Institute walk
+through project "Love Sandwiches"
+"""
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -15,3 +19,20 @@ SHEET = GSPREAD_CLIENT.open('Linguists_database')
 linguists = SHEET.worksheet('Linguists')
 data_lists = SHEET.worksheet('Data_lists')
 client_data = SHEET.worksheet('Client_data')
+
+
+def print_welcome():
+    welcome = """Welcome to Translate it! - a quick and easy way
+to find a lingusit and have your text translated.
+To start, select the language from the list below
+by choosing the corresponding number and then simply
+follow the instructions on the screen.
+"""
+    print(welcome)
+
+
+def main():
+    print_welcome()
+
+
+main()
