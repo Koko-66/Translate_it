c10bbea4d364aa758ef43d1a0a2ee35e9fd8534e
Koko-66 <52763151+Koko-66@users.noreply.github.com>
2021-08-05 22:39:41 +0100

    Add instructions and function to accept user input

--- word_counter.py
+++ word_counter.py
@@ -1,3 +1,29 @@
 """
 Takes in users input and calculates word count
 """
+
+
+print("\nCopy and paste your file here (Cmd+V on Linux/Mac\
+    \nor Ctrl+V on Windows). You can paste in multiple texts.\
+    \nOnce you're finished, press Enter and confirm upload with\
+    \nCtrl+d on Linux/Mac or Crtl+z on Windows.")
+
+
+def get_user_input():
+    """
+    Gets multiline input from the user and converts it to one string
+    """
+    # Code below taken from:
+    # https://stackoverflow.com/questions/34889012/how-to-paste-multiple-lines-of-text-into-python-input
+
+    text = []
+    try:
+        while True:
+            text.append(input())
+    except EOFError:
+        pass
+    text = "\n".join(text)
+    return text
+
+
+get_user_input()
