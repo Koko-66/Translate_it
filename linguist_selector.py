6d1a4d7cb39a40581529182d353b7fb9ffee1aa0
Koko-66 <52763151+Koko-66@users.noreply.github.com>
2021-08-17 20:42:40 +0100

    Add clacualte turnaround time function

--- linguist_selector.py
+++ linguist_selector.py
@@ -1,7 +1,10 @@
+import math
 """
 Create Linguist class.
 Print linguist list according to selection criteria.
 """
+
+WORKING_HOURS_DAY = 6
 
 
 class Linguist:
@@ -35,3 +38,34 @@ class Linguist:
         total_price = round(word_count * self.price, 2)
         return f"Total price: ${total_price}"
 
+    def calculate_turnaround_time(self, word_count):
+        """
+        Calculate time needed to translate the text
+        """
+        words_per_hour = round(self.turnaround/WORKING_HOURS_DAY)
+        turnaround_hours = round(word_count/words_per_hour)
+        if turnaround_hours < 6:
+            if turnaround_hours == 1 or word_count < words_per_hour:
+                return "Turnaround time: ca 1 hour."
+            else:
+                return f"Turnaround time: ca {turnaround_hours} hours."
+        else:
+            turnaround_days = turnaround_hours/6
+            full_days = math.trunc(turnaround_days)
+            turnaround_hours = round((turnaround_days - full_days) * 6)
+            if turnaround_days == 1:
+                return "Turnaround time: ca 1 day."
+            else:
+                if turnaround_hours == 0:
+                    return f"Turnaround time: ca {turnaround_days} day(s)."
+                else:
+                    return f"Turnaround time: ca \
+{full_days} days and {turnaround_hours} hours."
+
+
+koko = Linguist(2, 'koko', 'Polish', 3, 0.06, 1300, 2)
+
+pricing = koko.calculate_total_price(834)
+print(pricing)
+turnaround = koko.calculate_turnaround_time(1516)
+print(turnaround)
